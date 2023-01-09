7ed74db81f2c07b4e41b90b6343b80c95eee311f
HrutikPise <hrutikpise8@gmail.com>
2023-01-09 20:31:37 +0530

    Simple Caclulator operations

--- src/GitTry/AddTwoObjects.java
+++ src/GitTry/AddTwoObjects.java
@@ -5,8 +5,15 @@ public class AddTwoObjects {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int a=10,b=320;
-		int res = a + b;
-		System.out.println("Result = "+res);
+		System.out.println("Addition  = "+(a+b));
+
+		System.out.println("Multiplication = "+(a*b));
+		
+		System.out.println("Sub = "+(b-a));
+		
+		System.out.println("Div"+(b/a));
+		
+		
 	}
 
 }
