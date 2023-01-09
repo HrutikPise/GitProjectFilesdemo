283499434ec063743ecc3af60761131437cafa01
HrutikPise <hrutikpise8@gmail.com>
2023-01-09 20:21:57 +0530

    Addition of two objects

---
+++ src/GitTry/AddTwoObjects.java
@@ -0,0 +1,12 @@
+package GitTry;
+
+public class AddTwoObjects {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=10,b=320;
+		int res = a + b;
+		System.out.println("Result = "+res);
+	}
+
+}
